0eabe41a13609ba3373b4c52fad0cc0bd279b5ed
Abeed <abeedrahman@outlook.com>
2023-12-27 18:50:19 +0300

    Co-authored-by: ϻisռad Ϙasiϻ <Misnad@users.noreply.github.com>

--- getkeys.py
+++ getkeys.py
@@ -1,4 +1,6 @@
 from math import sqrt
+import random
+
 
 def is_prime(num):
     """Checks if argument is prime number.
@@ -15,8 +17,28 @@ def is_prime(num):
     return True
 
 
+def fi_N(p, q):
+    return (p-1) * (q-1)
+
+
+def gcd(a, b): 
+    if (a == 0 or b == 0): 
+        return 0
+    if (a == b):
+        return a
+    if (a > b):  
+        return gcd(a - b, b)   
+    else:
+        return gcd(a, b - a) 
+
+
+def coprime(a, b):
+    return gcd(a, b) == 1
+
+
 def print_line():
     print("-" * 11)
+
 
 print_line()
 p = int(input("Enter a five digit prime number: "))
@@ -31,11 +53,11 @@ while not is_prime(q):
     q = int(input("Not a prime number. Please enter again: "))
 
 
-n = p * q
-print("N = ", n)
+N = p * q
+fi = fi_N(p, q)
+print("N = ", N)
 
 
-def fi_N(p, q):
-    return (p-1) * (q-1)
-
-
+e = random.randint(10e6, 10e7)
+while not ( e < fi and coprime(e, fi) and coprime(e, N) ):
+    e = random.randint(10e6, 10e7)
